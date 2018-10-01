9743723bdc49d1aeb4b898eab33a52cc9d257c1d
cris-ech <i42runac@uco.es>
2018-10-01 21:22:12 +0200

    first commit

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const contractname = require('./build/'); //fix after 
+
+const provider = new HDWalletProvider( "nemonico" , 
+	"infura key"
+); //fix after
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(contractname.interface)
+  )
+    .deploy({ data: contractname.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
